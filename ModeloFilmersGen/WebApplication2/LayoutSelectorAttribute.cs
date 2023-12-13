90edbe79b79dfd25791ad4f729090c38bc95ed95
nuririera <nrvr1@gcloud.ua.es>
2023-12-13 19:20:34 +0100

    Sesión y layout

---
+++ ModeloFilmersGen/WebApplication2/LayoutSelectorAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+public class LayoutSelectorAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext filterContext)
+    {
+        var usuario = // Aquí obtienes tu usuario (dependiendo de tu lógica de autenticación)
+
+        if (usuario == null)
+        {
+            filterContext.Controller.ViewData["Layout"] = "~/Views/Shared/LayoutSinLogin.cshtml";
+        }
+        else
+        {
+            filterContext.BasicController.ViewData["Layout"] = "~/Views/Shared/TuLayoutNormal.cshtml";
+        }
+
+        base.OnActionExecuting(filterContext);
+    }
+}
